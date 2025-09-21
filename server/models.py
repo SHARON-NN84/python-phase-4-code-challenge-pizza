3d5abc5eb6bfbf917327b2a5b113e26353d58bfe
sharon <njerisharon84@gmail.com>
2025-09-21 21:34:54 +0300

    models.py

--- server/models.py
+++ server/models.py
@@ -42,3 +42,26 @@ class Pizza(db.Model, SerializerMixin):
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
+
+class RestaurantPizza(db.Model, SerializerMixin):
+    __tablename__ = "restaurant_pizzas"
+
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+
+    restaurant_id = db.Column(db.Integer, db.ForeignKey("restaurants.id"))
+    pizza_id = db.Column(db.Integer, db.ForeignKey("pizzas.id"))
+
+    restaurant = db.relationship("Restaurant", back_populates="restaurant_pizzas")
+    pizza = db.relationship("Pizza", back_populates="restaurant_pizzas")
+
+    serialize_rules = ("-restaurant.restaurant_pizzas", "-pizza.restaurant_pizzas")
+
+    @validates("price")
+    def validate_price(self, key, value):
+        if value < 1 or value > 30:
+            raise ValueError("Price must be between 1 and 30.")
+        return value
+
+    def __repr__(self):
+        return f"<RestaurantPizza ${self.price}>"
