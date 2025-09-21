b0c59c021f94dbb2092245cae4369eecfad38823
sharon <njerisharon84@gmail.com>
2025-09-21 21:29:06 +0300

    models.py

--- server/app.py
+++ server/app.py
@@ -24,6 +24,72 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = Restaurant.query.all()
+        return [r.to_dict(only=("id", "name", "address")) for r in restaurants], 200
+    
+api.add_resource(Restaurants, "/restaurants")
+
+class RestaurantById(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            return restaurant.to_dict(
+                only=("id", "name", "address", "restaurant_pizzas")
+            ), 200
+        return {"error": "Restaurant not found"}, 404
+    
+    def delete(self, id):
+        restaurant = Restaurant.query.get(id)
+        if restaurant:
+            db.session.delete(restaurant)
+            db.session.commit()
+            return "", 204
+        else:
+            return {"error": "Restaurant not found"}, 404
+    
+api.add_resource(RestaurantById, "/restaurants/<int:id>")
+
+class Pizzas(Resource):
+    def get(self):
+        pizzas = Pizza.query.all()
+        return [p.to_dict(only=("id", "name", "ingredients")) for p in pizzas], 200
+    
+api.add_resource(Pizzas, "/pizzas")
+
+class RestaurantPizzas(Resource):
+    def post(self):
+        data = request.get_json()
+
+        price = data.get("price")
+        pizza_id = data.get("pizza_id")
+        restaurant_id = data.get("restaurant_id")
+
+        pizza = Pizza.query.get(pizza_id)
+        restaurant = Restaurant.query.get(restaurant_id) 
+
+        if not pizza or not restaurant:
+            errors = []
+            if not pizza:
+                errors.append("Pizza not found")
+            if not restaurant:
+                errors.append("Restaurant not found")
+            return {"errors": errors}, 404
+        
+        try:
+            new_rp = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+            db.session.add(new_rp)
+            db.session.commit()
+        except ValueError:
+            return {"errors": ["validation errors"]}, 400
+        
+        return new_rp.to_dict(
+            only=("id", "price", "pizza_id", "restaurant_id", "pizza", "restaurant")
+        ), 201
+    
+api.add_resource(RestaurantPizzas, "/restaurant_pizzas")
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
